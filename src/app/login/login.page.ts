17a830f16944ac90e340a0877174a9ed493b6017
Hartikainen1996 <hartikainen-96@hotmail.com>
2020-04-26 11:50:07 +0300

    changes

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,38 +1,37 @@
-// Matias Kohanevic
-// 1800715
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthenticationService } from '../shared/authentication-service';
+// Eemil Hartikainen
+// 1800699
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthenticationService } from "../shared/authentication-service";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  selector: "app-login",
+  templateUrl: "./login.page.html",
+  styleUrls: ["./login.page.scss"],
 })
-
 export class LoginPage implements OnInit {
-
   constructor(
     public authService: AuthenticationService,
     public router: Router
-  ) { }
+  ) {}
 
-  ngOnInit() { }
+  ngOnInit() {}
 
   // Logging in
   logIn(email, password) {
-    this.authService.SignIn(email.value, password.value)
+    this.authService
+      .SignIn(email.value, password.value)
       .then((res) => {
         if (this.authService.isEmailVerified) {
           // Successful login navigates to this page
-          this.router.navigate(['welcome/introduction']);
+          this.router.navigate(["welcome/introduction"]);
         } else {
-          window.alert('Email is not verified');
+          window.alert("Email is not verified");
           return false;
         }
-      }).catch((error) => {
+      })
+      .catch((error) => {
         window.alert(error.message);
       });
   }
-
 }
